d09f84befd3165f9ef33d384e597f7d04a1d7917
939952154@qq.com <939952154@qq.com>
2020-06-12 11:19:39 +0800

    项目分包

---
+++ android/app/src/main/java/com/personal/wanandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.personal.wanandroid;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
